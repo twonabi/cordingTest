b7103e5f4f9e82012e137922ad3a2d3f69f9a247
jian <loveinansan1@gmail.com>
2024-06-24 19:31:52 +0900

    test 2 문제 003) 평균 구하기 - 세준이는 기말고사를 망쳤다. 그래서 점수를 조작해 집에 가져가기로 결심했다. 일단 세준이는 자기 점수 중 최댓값을 골랐다 그런 다음 최댓값을 M이라 할때 모즌 점수를 점수/M*100으로 고쳤다. 예를 들어 세준이의 최고점이 70점 수학 점수가 50점이라면 수학 점수는 50/70*100이므로 71.43점이다. 세준이의 성적을 이 방법으로 계산했을 때 새로운 평균을 구하는 프로그램을 작성하시오. 입력)  1번째 줄에 시험을 본 과목의 개수 N이 주어진다. 해당 값은 1,000보다 작서나 같다. 2번째 줄에 세준이의 현재 성적이 주어진다. 해당 값은 100보다 작거나 같은, 음이 아닌 정수이고, 적어도 1개의 값은 0보다 크다. 출력) 1번째 줄에 새로운 평균을 출력한다. 실제 정답과 출력값의 절대 오차 또는 상대 오차가 10-2승 이하이면 정답이다.
    슈도코드 작성하기)
    변수 N에 과목의 수 입력받기
    길이가 N인 1차우너 배열 A[]선언하기
    for(A[] 길이만큼 반복하기){
    A[i]에 각 점수 저장하기
    }
    for(A[] 길이만큼 반복하기) {
    최고점은 변수 max에, 총점은 변수 sum에 저장하기
    }
    sum * 100 / max / N 출력하기

---
+++ test/P1546.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.Scanner;
+
+public class P1546 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        long sum = 0;
+        long max = 0;
+
+        for (int i = 0; i < N; i++) {
+            int temp = sc.nextInt();
+            if(temp>max)max=temp;
+            sum = sum + temp;
+        }
+        System.out.println(sum*100.0/max/N);
+    }
+}
